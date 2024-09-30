e47e642e592d01f000a0b199f9b3d907b85b4f33
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-30 12:54:05 +0200

    Aggiunto main e costruttori

--- src/auto/Auto.java
+++ src/auto/Auto.java
@@ -2,7 +2,7 @@ package auto;
 
 public class Auto {
 
-    //Variabili d'Istanza
+    //Attributi
     private String marca;
     private String modello;
     private double prezzo;
@@ -17,4 +17,15 @@ public class Auto {
     public String getModello() { return modello; }
     public double getPrezzo() { return prezzo; }
 
+    //Costruttore
+    public Auto(){
+        marca=null;
+        modello=null;
+        prezzo=0.0;
+    }
+    public Auto(String marcaNew,String modelloNew, double prezzoNew){
+        marca=marcaNew;
+        modello=modelloNew;
+        prezzo=prezzoNew;
+    }
 }
--- src/auto/TestAuto.java
+++ src/auto/TestAuto.java
@@ -1,7 +1,36 @@
 package auto;
 
+import java.util.Scanner;
+
 public class TestAuto {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        //Inserire informzioni tramite set
+        Auto a1=new Auto();
+            System.out.println("AUTO 1\nInserisci marca");
+            a1.setMarca(in.nextLine());
+            System.out.println("Inserisci modello");
+            a1.setModello(in.nextLine());
+            System.out.println("Inserisci prezzo");
+            a1.setPrezzo(in.nextDouble());
+        in.nextLine();
+        Auto a2=new Auto("Fiat","Panda Disel",15000.0);
+        Auto a3=new Auto();
+            System.out.println("AUTO 3\nInserisci marca");
+            a3.setMarca(in.nextLine());
+            System.out.println("Inserisci modello");
+            a3.setModello(in.nextLine());
+            System.out.println("Inserisci prezzo");
+            a3.setPrezzo(in.nextDouble());
+        in.nextLine();
+        Auto a4=new Auto("Opel","Meriva Benzina",5000.0);
+
+        //Get
+        System.out.println("Auto 1:"+a1.getMarca()+" ; "+a1.getModello()+" ; "+a1.getPrezzo());
+        System.out.println("Auto 2:"+a2.getMarca()+" ; "+a2.getModello()+" ; "+a2.getPrezzo());
+        System.out.println("Auto 3:"+a3.getMarca()+" ; "+a3.getModello()+" ; "+a3.getPrezzo());
+        System.out.println("Auto 4:"+a4.getMarca()+" ; "+a4.getModello()+" ; "+a4.getPrezzo());
 
     }
 }
