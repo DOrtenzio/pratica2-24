cc7fc5aa23877748714989adabb23a7ffd84198f
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-30 12:19:45 +0200

    Aggiunte variabili d'istanza

---
+++ src/auto/Auto.java
@@ -0,0 +1,9 @@
+package auto;
+
+public class Auto {
+    //Variabili d'Istanza
+    private String marca;
+    private String modello;
+    private double prezzo;
+
+}
---
+++ src/auto/TestAuto.java
@@ -0,0 +1,7 @@
+package auto;
+
+public class TestAuto {
+    public static void main(String[] args) {
+
+    }
+}
