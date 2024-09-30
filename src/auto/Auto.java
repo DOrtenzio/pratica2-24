175c0140e5291dc9eee16ab93eee2f6139f629f5
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-30 12:24:21 +0200

    Aggiunti metodi get

--- src/auto/Auto.java
+++ src/auto/Auto.java
@@ -1,6 +1,7 @@
 package auto;
 
 public class Auto {
+
     //Variabili d'Istanza
     private String marca;
     private String modello;
@@ -10,4 +11,10 @@ public class Auto {
     public void setMarca( String marcaNew ){ marca=marcaNew; }
     public void setModello( String modelloNew ){ modello=modelloNew; }
     public void setPrezzo( double prezzoNew ){ prezzo=prezzoNew; }
+
+    //Get
+    public String getMarca() { return marca; }
+    public String getModello() { return modello; }
+    public double getPrezzo() { return prezzo; }
+
 }
